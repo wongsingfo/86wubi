1aecfab5e0bd36a8c64789b3d3515fe4c36c4dd5
Chengke Wang <chengke@pku.edu.cn>
2022-02-10 16:22:46 +0800

    Hide prompt for stop words

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function WubiInput() {
 	const whitespaceCharacters = [' ', ' ',
-	  '\b', '\t', '\n', '\v', '\f', '\r', `\"`, `\'`, `\\`,
-	  '\u0008', '\u0009', '\u000A', '\u000B', '\u000C',
-	'\u000D', '\u0020','\u0022', '\u0027', '\u005C',
-	'\u00A0', '\u2028', '\u2029', '\uFEFF'];
+		'\b', '\t', '\n', '\v', '\f', '\r', `\"`, `\'`, `\\`,
+		'\u0008', '\u0009', '\u000A', '\u000B', '\u000C',
+		'\u000D', '\u0020', '\u0022', '\u0027', '\u005C',
+		'\u00A0', '\u2028', '\u2029', '\uFEFF'];
+
+	const stop_words = '\'"()[]{}<>.,:;!?*&^%$#@~`-_=+/\\|' +
+		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+		'0123456789' +
+		'，。“”《》——！？：、‘’';
 
 	function isSpace(str) {
 		for (const space of whitespaceCharacters) {
@@ -14,10 +19,14 @@ function WubiInput() {
 		return false;
 	}
 
+	function isStopWord(str) {
+		return stop_words.indexOf(str) > -1;
+	}
+
 	function moveInputBox() {
 		const cursor = document.getElementById('cursor');
-		const { left, top } = cursor.getBoundingClientRect();
-		const { offsetLeft, offsetTop, offsetHeight } = cursor;
+		const {left, top} = cursor.getBoundingClientRect();
+		const {offsetLeft, offsetTop, offsetHeight} = cursor;
 		const inputbox = document.getElementById('inputbox');
 		inputbox.style.top = offsetTop + offsetHeight;
 		inputbox.style.left = offsetLeft;
@@ -26,9 +35,11 @@ function WubiInput() {
 	}
 
 	return {
-		target: "冰灯是流行于中国北方的一种古老的民间艺术形式。",
+		target: "Loading...",
 		progress: Number(localStorage.getItem("progress")) || 0,
 		inputtext: '',
+
+		isStopWord,
 
 		getText() {
 			fetch('text.txt')
@@ -59,7 +70,7 @@ function WubiInput() {
 			if (progress >= target.length) {
 				return;
 			}
-			
+
 			let hasEnter = false;
 			let hasSpace = false;
 			let newInputtext = '';
